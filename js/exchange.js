ab46a4c2ceb9c999ef8d0d0398a57f69b62cca28
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-08-14 12:37:27 +0200

    Change error handling

--- js/exchange.js
+++ js/exchange.js
@@ -14,22 +14,8 @@ function exchange(username, password, email, type, amount, address, coin) {
             } else {
                 alert(data.message);
             }
-        }).fail(function() {
-        alert("Exchange API is currently unavailable but the exchange request can be sent by e-mail, " +
-            "do you want to submit the request by emailing us? " +
-            "Click OK to open your mail program with pre-filled data"
-        )
-        window.open(`mailto:duino.coin@gmail.com?subject=DUCO EXCHANGE&body=` +
-            `Please check if the blanks below are correct ` +
-            `and send an e-mail so we receive your exchange request:%0D%0A ` +
-            `%0D%0AYour Duino-Coin Username: ` + username +
-            `%0D%0AYour Duino-Coin Password: ` + password +
-            `%0D%0AE-mail associated with your Duino-Coin account: ` + email +
-            `%0D%0AAre you selling or buying DUCO? ` + type +
-            `%0D%0AHow much DUCO are you selling/buying? ` + amount +
-            `%0D%0AYour BCH wallet address: ` + address +
-            `%0D%0A` +
-            `%0D%0A2019-2021 DUCO Exchange`
-        )
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert('Error submitting exchange request: ' + textStatus); 
+        })
     });
 }
